ef87bd597cb27da8549af59091b165fa70af3598
KyungBin7 <jkb12377@naver.com>
2023-07-08 19:21:32 +0900

    create detailed post page

--- car_sharing/main/urls.py
+++ car_sharing/main/urls.py
@@ -1,9 +1,15 @@
+from django.contrib import admin
 from django.urls import path
 from .views import *
 
 app_name='main'
 
 urlpatterns=[
-    path('',index),
-    path('board/',board),
+    path('admin/', admin.site.urls),
+    # 웹사이트의 첫화면은 index 페이지이다 + URL이름은 index이다
+    path('', index, name='index'),
+    # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
+    path('board/', board, name='blog'),
+    # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
+    path('board/<int:pk>', posting, name="posting"),
 ]
