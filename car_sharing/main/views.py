51d7202c6b6207f390ab057994853a338d88497c
KyungBin7 <jkb12377@naver.com>
2023-07-08 19:09:42 +0900

    create board_list_page

--- car_sharing/main/views.py
+++ car_sharing/main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+# View에 Model(Post 게시글) 가져오기
+from .models import Post
 
 # Create your views here.
 def index(request):
     return render(request,'main/index.html')
 
 def board(request):
-    return render(request, 'main/board.html')
+    # 모든 Post를 가져와 postlist에 저장합니다
+    postlist = Post.objects.all()
+    # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다
+    return render(request, 'main/board.html', {'postlist': postlist})
+    #return render(request, 'main/board.html')
